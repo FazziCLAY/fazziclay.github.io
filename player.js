05deb0c67b08194aba0ed7876dc4193962b1a576
FazziCLAY <fazziclay@gmail.com>
2024-10-20 20:13:24 +0300

    Update player.js

--- player.js
+++ player.js
@@ -1,1 +1,19 @@
+const objText = null;
 
+function updatePlayer() {
+  fetch("https://fazziclay.com/api/v1/person/status/cuteTextPlayer")
+        .then(response => {
+            objText.innerHTML = response;
+        })
+}
+
+
+function startCutePlayer() {
+  objText = document.getElementById("cute-text-player")
+  
+  updatePlayer();
+  
+  setInterval(function() {
+        updatePlayer()
+    }, 1000)
+}
